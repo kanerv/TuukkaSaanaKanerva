fc24b67cdd119bcfb5a0cbded2884e15ee121462
Saana Hyttinen <saana.hyttinen@gmail.com>
2021-01-21 13:49:52 +0200

    corrected the file

--- testi.py
+++ testi.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 print("Bye World!")
-=======
 print("Hellos World!")
-<<<<<<< HEAD
 print("Never again!")
-=======
->>>>>>> 733e3230a521860ced840fbe25d26c516bb4ec45
-print("Hello!")Ã
+print("Hello!")
 print("k")
->>>>>>> 2f0b79b3a0f1ac3de37ee610780e5d0a0f95b8ce
