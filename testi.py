e69c71c0200f06048eb3ea6bbc514189a6215ca4
TuukkaOT <tuutor@protonmail.com>
2021-01-21 14:08:58 +0200

    postin turhat

--- testi.py
+++ testi.py
@@ -3,8 +3,3 @@ print("Hellos World!")
 print("Never again!")
 print("Hello!")
 print("k")
-<<<<<<< HEAD
->>>>>>> 2f0b79b3a0f1ac3de37ee610780e5d0a0f95b8ce
-====
-=======
->>>>>>> fc24b67cdd119bcfb5a0cbded2884e15ee121462
