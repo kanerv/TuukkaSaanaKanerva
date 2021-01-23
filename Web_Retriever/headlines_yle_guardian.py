e55165939aa5aca7216363f2e7c445a6d475c51b
TuukkaOT <tuutor@protonmail.com>
2021-01-23 14:18:08 +0200

    fixed a small glitch that duplicated headlines when refeshing.

--- Web_Retriever/headlines_yle_guardian.py
+++ Web_Retriever/headlines_yle_guardian.py
@@ -4,24 +4,26 @@ prints the ten top headlines."""
 import nltk, requests, datetime, webbrowser
 from bs4 import BeautifulSoup
 from termcolor import colored
-lista = []
-cleanlist = []
 
 
 def main():
     open_site = "refresh"
     while open_site == "refresh":
+        lista = []
+        cleanlist = []
         url = "https://yle.fi/uutiset/tuoreimmat"
+        #next lines of code extract the source code of the website and analyse its contents
         html = requests.get(url)
         page = html.content
         soup = BeautifulSoup(page, 'html.parser')
+        #this for-loop finds all cases of the html class that have headlines.
         for i in soup.find_all(class_="Typography__StyledResponsiveTypography-sc-1his0m9-1 dxkrlB link-accent"):
-            i = i.get_text()
-            i = str(i).strip()
-            if len(lista) < 10:
+            i = i.get_text() #cleans html markup
+            i = str(i).strip() #cleans extra white rows
+            if len(lista) < 10: #appends 10 first headlines to a list
                 lista.append(i)
         for i in lista:
-            i = str(i).replace('  ',' ')
+            i = str(i).replace('  ',' ') #cleans extra white space and hyphenation from the headlines
             i = str(i).replace('­','')
             cleanlist.append(i)
         print()
@@ -32,12 +34,13 @@ def main():
         print (colored("    | |  | |____| |____ _| |_ ____) | | \ \  / ____ \| |__| || || |__| |", "blue"))
         print (colored("    |_|  |______|______|_____|_____/|_|  \_\/_/    \_\_____/_____\____/ ", "blue"))
         print()
-        now = datetime.datetime.now()    
-        print("Kello on tällä hetkellä", now.strftime("%Y-%m-%d %H:%M:%S"))
+        now = datetime.datetime.now() #creates a variable that contains current time
+        print("Kello on tällä hetkellä", now.strftime("%Y-%m-%d %H:%M:%S")) #print current time
         print()
         print("Tuoreimmat kymmenen uutista ovat:\n")
-        print(*cleanlist, sep ='\n*\n')
+        print(*cleanlist, sep ='\n*\n') #prints headlines on separate rows
         
+        """The following code is exactly the same as the first half until "open_site" """
         lista2 = []
         cleanlist2 = []
         url = "https://www.theguardian.com/world"
@@ -68,6 +71,7 @@ def main():
         print()
         print()
         print("***********")
+        #Now the program asks for input on what the user wants to do, i.e. open a website, refresh the headlines, or quit.
         open_site = input("If you want to open YLE, Guardian or both websites, write \"yle\", \"guardian\", or \"both\", respectively. \nTo refresh, write \"refresh\". To quit, press enter: ") 
         if open_site == "yle":
             webbrowser.open('https://www.yle.fi/uutiset/tuoreimmat', new=2)
@@ -76,6 +80,4 @@ def main():
         elif open_site == "both":
             webbrowser.open('https://www.yle.fi/uutiset/tuoreimmat', new=2)
             webbrowser.open('https://www.theguardian.com/world', new=2)
-       # else:
-        #    return
 main    ()  
