873e44986f4c343a6aee69739320c76729d598e3
TuukkaOT <tuutor@protonmail.com>
2021-01-23 09:55:50 +0200

    Added print command for current time and date.

--- Web_Retriever/guardian_headlines.py
+++ Web_Retriever/guardian_headlines.py
@@ -1,7 +1,7 @@
 """This program accesses the Guardian World News website, retrieves text from it and
 prints the ten top headlines."""
 
-import nltk, requests
+import nltk, requests, datetime
 from bs4 import BeautifulSoup
 lista = []
 cleanlist = []
@@ -20,5 +20,10 @@ def main():
     for i in lista:
         i = str(i).replace('  ',' ')
         cleanlist.append(i)
+    
+    now = datetime.datetime.now()
+    print("Current date and time is", now.strftime("%Y-%m-%d %H:%M:%S"))
+    print()
+    print("The first ten headlines from Guardian World News website are:\n")
     print(*cleanlist, sep ='\n')
 main()
