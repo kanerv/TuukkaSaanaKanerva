55c8c8437b05240351539bc615e5aa4c1e28d8b1
TuukkaOT <tuutor@protonmail.com>
2021-01-24 14:13:02 +0200

    #6 tweaked things  around row 16 so the app extracts the links from the html code

--- Test_files/url_retriever.py
+++ Test_files/url_retriever.py
@@ -13,7 +13,7 @@ def main():
 	soup = BeautifulSoup(page, 'html.parser')
     
 	#this for-loop extracts the url links corresponding the headlines
-	for link in soup.find_all(class_="Typography__StyledResponsiveTypography-sc-1his0m9-1 dxkrlB link-accent"):
+	for link in soup.find_all(class_="GridSystem__GridRow-sc-15162af-1 ljWZzL visitableLink"):
 		link = link.get("href")
 		if len(linkit) < 10: #appends 10 first headlines to a list
 			linkit.append(link)
