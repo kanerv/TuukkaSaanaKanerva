8025525f55c915a99a2934a8bd8495fc157ac3c9
TuukkaOT <tuutor@protonmail.com>
2021-01-28 12:04:14 +0200

    took out some code

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -16,34 +16,3 @@ def main():
 
     except OSError:
         print("Could not find the file.")
-
-print("Term-document matrix: (?)\n")
-print(sparse_matrix)
-
-dense_matrix = sparse_matrix.todense()
-
-print("Term-document matrix: (?)\n")
-print(dense_matrix)
-
-td_matrix = dense_matrix.T   # .T transposes the matrix
-
-print("Term-document matrix:\n")
-print(td_matrix)
-
-print("\nIDX -> terms mapping:\n")
-print(cv.get_feature_names())
-
-terms = cv.get_feature_names()
-
-print("First term (with row index 0):", terms[0])
-print("Third term (with row index 2):", terms[2])
-
-print("\nterm -> IDX mapping:\n")
-print(cv.vocabulary_) # note the _ at the end
-
-print("Row index of 'example':", cv.vocabulary_["example"])
-print("Row index of 'silly':", cv.vocabulary_["silly"])
-<<<<<<< HEAD
-=======
-
->>>>>>> df81a73d0091341a6b7530ee4186573a8bde65cf
