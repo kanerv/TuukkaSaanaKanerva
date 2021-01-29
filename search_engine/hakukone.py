3ab0683c950b174b5051b73f4fafe999bb159a3c
TuukkaOT <tuutor@protonmail.com>
2021-01-29 08:28:44 +0200

    #10 made the app handle invalid terms.

--- search_engine/hakukone.py
+++ search_engine/hakukone.py
@@ -43,15 +43,21 @@ def main():
     def test_query(query):
         print("Query: '" + query + "'")
         print("Rewritten:", rewrite_query(query))
+
         #print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
 
         #Printing the matches instead of ones and zeros
-        hits_matrix = eval(rewrite_query(query))
-        hits_list = list(hits_matrix.nonzero()[1])
-        for i, doc_idx in enumerate(hits_list):
-            print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
-        print()
+        try:
+            hits_matrix = eval(rewrite_query(query))
+            hits_list = list(hits_matrix.nonzero()[1])
+            for i, doc_idx in enumerate(hits_list):
+                print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
+                print()
 
+        except KeyError:
+            print("Search term not found. No Matching doc.")
+            
+        
 
 
     query = "?"
