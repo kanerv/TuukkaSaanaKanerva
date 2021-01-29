d1b4a1bbde9b6307d98d498734c350d1bffd3b2d
TuukkaOT <tuutor@protonmail.com>
2021-01-29 14:47:46 +0200

    Made the changes to scale up the app. Added documentation and deleted dead code.

--- search_engine/hakukone.py
+++ search_engine/hakukone.py
@@ -15,26 +15,22 @@ def main():
 
         documents = []
 
-        pattern = r'(?u)\b\w+\b'
+        pattern = r'(?u)\b\w+\b' #a new regex that takes into account tokens comprised of a singe alphanumerical character
  
-        file = open("enwiki-20181001-corpus.100-articles.txt", "r")
-        file_variable = file.read()
-        file_variable = file_variable.split("</article>")
-        for i in file_variable:
-            documents.append(i)
+        file = open("enwiki-20181001-corpus.100-articles.txt", "r") #Opens the file
+        file_variable = file.read() #Reads the contents
+        documents = file_variable.split("</article>") #splits the file into a list at </article>
         file.close()
 
         
         cv = CountVectorizer(lowercase=True, binary=True, token_pattern=pattern)
+        global dense_matrix, sparse_td_matrix,td_matrix, t2i
         sparse_matrix = cv.fit_transform(documents)
-
         dense_matrix = sparse_matrix.todense()
-
-        global td_matrix, t2i
+        sparse_td_matrix = sparse_matrix.T.tocsr()
         td_matrix = dense_matrix.T   # .T transposes the matrix
 
         terms = cv.get_feature_names()
-
         t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
 
         # Operators and/AND, or/OR, not/NOT become &, |, 1 -
@@ -47,7 +43,8 @@ def main():
             "(": "(", ")": ")"}          # operator replacements
                
         def rewrite_token(t):
-            return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) # Can you figure out what happens here?
+            return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t)) # Make retrieved rows dense
+
 
         def rewrite_query(query): # rewrite every token in the query
             return " ".join(rewrite_token(t) for t in query.split())
@@ -56,9 +53,6 @@ def main():
             print("Query: '" + query + "'")
             print("Rewritten:", rewrite_query(query))
 
-            #print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
-
-            #Printing the matches instead of ones and zeros
             try:
                 hits_matrix = eval(rewrite_query(query))
                 hits_list = list(hits_matrix.nonzero()[1])
@@ -67,10 +61,7 @@ def main():
                     print()
 
             except KeyError:
-                print("Search term not found. No Matching doc.")
-                
-            
-
+                print("Search term not found. No Matching doc.")         
 
         query = "?"
         while query != "":
@@ -80,29 +71,6 @@ def main():
                 test_query(query)
             else:
                 print("You did not enter a query, bye!")
-
-            #test_query("example AND NOT nothing")
-            #test_query("NOT example OR great")
-            #test_query("( NOT example OR great ) AND nothing") # AND, OR, NOT can be written either in ALLCAPS
-            #test_query("( not example or great ) and nothing") # ... or all small letters
-            #test_query("not example and not nothing")
-
-            #sparse_td_matrix = sparse_matrix.T.tocsr() --> scaling up osaan
-
-            #def rewrite_token(t):
-                #return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t)) # Make retrieved rows dense
-
-
-                #hits_matrix = eval(rewrite_query("NOT example OR great"))
-
-                #hits_list = list(hits_matrix.nonzero()[1])
-
-                #for doc_idx in hits_list:
-                    #print("Matching doc:", documents[doc_idx])
-
-                #for i, doc_idx in enumerate(hits_list):
-                    #print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
-
 
     except OSError:
         print("Could not find the file.")
