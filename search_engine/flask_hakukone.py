0db3db2992f5395efdd8de76bc2fcdf792261064
tt <tuutor@protonmail.com>
2021-02-15 19:18:15 +0200

    #32 added . and quotation mark to regex and made a try-except for keyerror so the app doesn't crash and it works pretty okay with wildcard as well.

--- search_engine/flask_hakukone.py
+++ search_engine/flask_hakukone.py
@@ -105,37 +105,41 @@ def test_query(query):
     matches = []
     
     """Ceates a matric and term-dictionary index"""
-    tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", token_pattern=r"\b\w\w+\-*\'*\w*\b")
+    tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", token_pattern=r"\b\w\w+\-*\'*\.*\"*\w*\b")
     global tf_matrix, terms, t2i
     tf_matrix = tfv.fit_transform(documents).T.todense()
     terms = tfv.get_feature_names()
     t2i = tfv.vocabulary_  # shorter notation: t2i = term-to-index
-    hits_list = np.array(tf_matrix[t2i[query]])[0]
-    hits_and_doc_ids = [ (hits, i) for i, hits in enumerate(hits_list) if hits > 0 ]
+    try:
+        hits_list = np.array(tf_matrix[t2i[query]])[0]        
+        hits_and_doc_ids = [ (hits, i) for i, hits in enumerate(hits_list) if hits > 0 ]
 
-    ranked_hits_and_doc_ids = sorted(hits_and_doc_ids, reverse=True)
+        ranked_hits_and_doc_ids = sorted(hits_and_doc_ids, reverse=True)
 
-    #cosine similarity:
-    query_vec = tfv.transform([query]).todense()
-    scores = np.dot(query_vec, tf_matrix)                
-    ranked_scores_and_doc_ids = \
-    sorted([ (score, i) for i, score in enumerate(np.array(scores)[0]) if score > 0], reverse=True)
-    #print("There are ", len(ranked_scores_and_doc_ids), " documents matching your query:")
+        #cosine similarity:
+        query_vec = tfv.transform([query]).todense()
+        scores = np.dot(query_vec, tf_matrix)                
+        ranked_scores_and_doc_ids = \
+        sorted([ (score, i) for i, score in enumerate(np.array(scores)[0]) if score > 0], reverse=True)
+        #print("There are ", len(ranked_scores_and_doc_ids), " documents matching your query:")
 
-    # Trying to output something if the search doesn't match anything, NOT WORKING YET
-    if len(ranked_scores_and_doc_ids) == 0:
-        line = "No matches found"
-        matches.append(line)
-    else:
-        for score, i in ranked_scores_and_doc_ids:
-            score = "{:.4f}".format(score)
-            snippet_index = documents[i].lower().find(query)    #Finds an index for a snippet for printing results.
-            header = documents[i].split('"')[1]                #Finds the header of an article for printing results.
-            header = str(header)
-            snippet = "..."+documents[i][snippet_index:snippet_index+100]+"..."
-            snippet = str(snippet)
-            line = "The score of " + query + " is "+ score + " in the document named: " + header + "\n" + "Here is a snippet: " + snippet
+        # Trying to output something if the search doesn't match anything, NOT WORKING YET
+        if len(ranked_scores_and_doc_ids) == 0:
+            line = "No matches found"
             matches.append(line)
-    #print("The score of " + query + " is {:.4f} in the document named: {:s}. Here is a snippet: ...{:s}...\n***".format(score, header, documents[i][snippet_index:snippet_index+100]))
+        else:
+            for score, i in ranked_scores_and_doc_ids:
+                score = "{:.4f}".format(score)
+                snippet_index = documents[i].lower().find(query)    #Finds an index for a snippet for printing results.
+                header = documents[i].split('"')[1]                #Finds the header of an article for printing results.
+                header = str(header)
+                snippet = "..."+documents[i][snippet_index:snippet_index+100]+"..."
+                snippet = str(snippet)
+                line = "The score of " + query + " is "+ score + " in the document named: " + header + "\n" + "Here is a snippet: " + snippet
+                matches.append(line)
+        #print("The score of " + query + " is {:.4f} in the document named: {:s}. Here is a snippet: ...{:s}...\n***".format(score, header, documents[i][snippet_index:snippet_index+100]))
+    except KeyError:
+        line = "Search term not found."
+        matches.append(line)
     return matches
     
