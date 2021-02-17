648fc063f50d807dd055b7d55199224a8bf2a63d
Kanerva Koskinen <kanerva@kanervas.local>
2021-02-17 20:38:05 +0200

    #28 I fixed the double printing by overwriting the documents variable before the stem() or relevance() functions are called. #30 I copied the line telling the number of matching documents form our previous program.

--- search_engine/flask_hakukone.py
+++ search_engine/flask_hakukone.py
@@ -30,16 +30,19 @@ def search():
         if choice == "stem":                            #(dead code)re.match(r'\w+_s\b', query):             #Recognizes stem searches
             query = query.lower()
             query = query + "_s"
+            documents = []
             documents = stem(text_string)
             matches = test_query(query)
 
         elif choice == "wildcard":                      #(dead code)re.match(r'\w+\*', query):             #Recognizes wildcard queries that end with a wildcard
             query = query.lower()
+            documents = []
             documents = relevance(text_string)
             matches = test_wcquery(query)
             
         elif choice ==  "exact":                            #!= "":
             query = query.lower()
+            documents = []
             documents = relevance(text_string)
             matches = test_query(query)
         
@@ -51,9 +54,9 @@ def search():
     #Render index.html with matches variable
     return render_template('index.html', matches=matches)
 
-def relevance(documents_in):
-            
-    documents_pre = documents_in.split("</article>") #splits the file into a list at </article>
+
+def relevance(documents_str):            
+    documents_pre = documents_str.split("</article>") #splits the file into a list at </article>
     for i in documents_pre:
         i = re.sub("<article name=", "", i)
         i = re.sub(">", "", i)
@@ -88,34 +91,39 @@ def stem(documents_in):
     return documents
 
 
+"""search function for exact and stem search"""
 def test_query(query):
     matches = []
     
-    """Ceates a matric and term-dictionary index"""
+    """Ceates a matric and a term vocabulary"""
     tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", token_pattern=r"\b\w\w+\-*\'*\.*\"*\w*\b")
     global tf_matrix, terms, t2i
     tf_matrix = tfv.fit_transform(documents).T.todense()
     terms = tfv.get_feature_names()
 
-    if query in terms:
-        t2i = tfv.vocabulary_  # shorter notation: t2i = term-to-index
-        hits_list = np.array(tf_matrix[t2i[query]])[0]        
-        hits_and_doc_ids = [ (hits, i) for i, hits in enumerate(hits_list) if hits > 0 ]
+    if query in terms:      #if query is found in the data
+        
+        """Creates a term-dictionary index and finds matching documents"""
+        #t2i = tfv.vocabulary_  # shorter notation: t2i = term-to-index
+        #hits_list = np.array(tf_matrix[t2i[query]])[0]        
+        #hits_and_doc_ids = [ (hits, i) for i, hits in enumerate(hits_list) if hits > 0 ]
+        #ranked_hits_and_doc_ids = sorted(hits_and_doc_ids, reverse=True)
 
-        ranked_hits_and_doc_ids = sorted(hits_and_doc_ids, reverse=True)
-
-        #cosine similarity:
+        """Creates a vector from the query, finds matching documents and ranks them"""
         query_vec = tfv.transform([query]).todense()
         scores = np.dot(query_vec, tf_matrix)                
         ranked_scores_and_doc_ids = \
         sorted([ (score, i) for i, score in enumerate(np.array(scores)[0]) if score > 0], reverse=True)
-        #print("There are ", len(ranked_scores_and_doc_ids), " documents matching your query:")
-        
+
+        """Finds the number of matched documents for printing"""
+        line = "There are " + str(len(ranked_scores_and_doc_ids)) + " documents matching your query:"
+        matches.append(line)
+
+        """Finds information for the printing"""
         for score, i in ranked_scores_and_doc_ids:
             score = "{:.4f}".format(score)
             query_match = re.search(r'\b' + query + r'\b', documents[i].lower())  #Trying to make the find() function to match only exact word like 'cat' and not 'publiCATion'
-            #snippet_index = documents[i].lower().find(query)    #Finds an index for a snippet for printing results.
-            snippet_index = query_match.start()
+            snippet_index = query_match.start()                 #Finds an index for a snippet for printing results.
             header = documents[i].split('"')[1]                #Finds the header of an article for printing results.
             header = str(header)
             snippet = "..."+documents[i][snippet_index:snippet_index+100]+"..."
@@ -124,15 +132,17 @@ def test_query(query):
             matches.append(line)
         #print("The score of " + query + " is {:.4f} in the document named: {:s}. Here is a snippet: ...{:s}...\n***".format(score, header, documents[i][snippet_index:snippet_index+100]))
 
-    else:
-        line = "Search term " + query + "not found."
+    else:       #if query is not found in the data
+        line = "Search term " + query + " not found."
         matches.append(line)
         
     return matches
 
-#search function for wildcard queries
+"""search function for wildcard queries"""
 def test_wcquery(query):
     matches = []
+
+    """Ceates a matric, a term vocabulary and a list of words matching the wildcard query"""
     tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", token_pattern=r"\b\w\w+\-*\'*\w*\b")
     global tf_matrix, terms, t2i
     tf_matrix = tfv.fit_transform(documents).T.todense()
@@ -140,13 +150,20 @@ def test_wcquery(query):
     wc_query = query+".+"
     wc_words = [w for w in terms if re.fullmatch(wc_query, w)]      #this line is copied from the group "wewhoshallnotbenamed"
     
-    if wc_words:
+    if wc_words:        #if words matching the query exist
+
+        """Creates a vector from the words matching the wildcard query, finds matching documents and ranks them"""
         new_query_string = " ".join(wc_words)
         query_vec = tfv.transform([new_query_string]).todense()
         scores = np.dot(query_vec, tf_matrix)                
         ranked_scores_and_doc_ids = \
         sorted([ (score, i) for i, score in enumerate(np.array(scores)[0]) if score > 0], reverse=True)
 
+        """Finds the number of matched documents for printing"""
+        line = "There are " + str(len(ranked_scores_and_doc_ids)) + " documents matching your query:"
+        matches.append(line)
+
+        """Finds information for the printing"""
         for score, i in ranked_scores_and_doc_ids:
             score = "{:.4f}".format(score)
             snippet_index = documents[i].lower().find(query)    #Finds an index for a snippet for printing results.
@@ -157,10 +174,9 @@ def test_wcquery(query):
             line = "The score of " + query + " is "+ score + " in the document named: " + header + "\n" + "Here is a snippet: " + snippet
             matches.append(line)
             
-    else:
+    else:       #if there are no words matching the query
         line = "No matches for wildcard search " + query
         matches.append(line)
         print()
         
-    print(matches)
     return matches
