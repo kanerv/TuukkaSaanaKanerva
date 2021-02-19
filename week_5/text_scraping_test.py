00d0f5ccea836923dda61f82fd37e3b7c21fe3bb
tt <tuutor@protonmail.com>
2021-02-19 08:23:41 +0200

    #37 made the app print the right critics consensuses and the names of the films.

--- week_5/text_scraping_test.py
+++ week_5/text_scraping_test.py
@@ -15,26 +15,33 @@ import re
 def main():
         review_links = [] #links to individual film pages
         preview = [] #links to critics consensuses
+        titles = []
         url = "https://www.rottentomatoes.com/top/bestofrt/" #best 100 films url
         parser = "html.parser"
         html = requests.get(url)
         soup = BeautifulSoup(html.text, parser) #parses the 100 best films page
+        
         json_content = json.loads("".join(soup.find("script", {"type":"application/ld+json"}).contents)) #loads the json script from parsed html page into a python dictionary
         for i in json_content['itemListElement']: #iterates through dictionary
             if len(review_links) < 10: #caps list at 10 url's
                 review_links.append(i['url']) #extracts the url from the dictionary
+                
+
         
         for i in review_links: #iterates through links to individual pages
             link = requests.get(i) #retrieves the URLs
             page_content = link.content #extracts URL contents from individual film pages
             soup = BeautifulSoup(page_content, 'html.parser') #parses the film page
-            for i in soup.find('span', attrs={'data-qa': 'critics-consensus'}): #looks for the tagging for critics consensus
-                i = i.string
+            for i in soup.find_all(class_="what-to-know__section-body"): #looks for the tagging for critics consensus
+                i = i.get_text() #extracts only the text int he paragraph
                 preview.append(i)
+            for c in soup.find("title"): #extracts page title
+                c = re.sub("- Rotten Tomatoes", "", c) #resubs everything else than film's name
+                titles.append(c)
                 
                 
-        for i, (y, z) in enumerate(zip(review_links, preview)):
-            print("\nURL:", y, "\nPreview: ", z, "\n*")
+        for i, (k, y, z) in enumerate(zip(titles, review_links, preview)):
+            print("Film: ", k, "\nURL:", y, "\nPreview: ", z, "\n*")
         print("***********")
         print()
                     
