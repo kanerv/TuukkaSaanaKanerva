31ce7c899d18b4b33b546f53b2aebbe0da2ce6bb
tt <tuutor@protonmail.com>
2021-03-01 15:47:15 +0200

    #63 Added possibility to choose year for mining and to append the mined data after existing data.

--- final_project/text_scraping_list_version.py
+++ final_project/text_scraping_list_version.py
@@ -21,38 +21,49 @@ def main():
         review_links = [] #links to individual film pages
         preview = [] #links to critics consensuses
         titles = []
-
-        url = "https://www.rottentomatoes.com/top/bestofrt/?year=2020" #best 100 films 2016 url
-        parser = "html.parser"
-        html = requests.get(url, headers)
-        soup = BeautifulSoup(html.text, parser) #parses the 100 best films page
-        f = open("text_data_list.txt", "a")
-        json_content = json.loads("".join(soup.find("script", {"type":"application/ld+json"}).contents)) #loads the json script from parsed html page into a python dictionary
-        for i in json_content['itemListElement']: #iterates through dictionary
-            review_links.append(i['url']) #extracts the url from the dictionary
+        question = input("Which year do you want to scrape? a)2016 b)2017 c)2018 d)2019 e)2020?")
+        while question != "":
+                if question == "a":
+                        url = "https://www.rottentomatoes.com/top/bestofrt/?year=2016" #best 100 films 2016 url
+                elif question == "b":
+                        url = "https://www.rottentomatoes.com/top/bestofrt/?year=2017"
+                elif question == "c":
+                        url = "https://www.rottentomatoes.com/top/bestofrt/?year=2018"
+                elif question == "d":
+                        url = "https://www.rottentomatoes.com/top/bestofrt/?year=2019"
+                elif question == "e":
+                        url = "https://www.rottentomatoes.com/top/bestofrt/?year=2020"
+                        
+                parser = "html.parser"
+                html = requests.get(url, headers)
+                soup = BeautifulSoup(html.text, parser) #parses the 100 best films page
+                f = open("text_data_list.txt", "a")
+                json_content = json.loads("".join(soup.find("script", {"type":"application/ld+json"}).contents)) #loads the json script from parsed html page into a python dictionary
+                for i in json_content['itemListElement']: #iterates through dictionary
+                    review_links.append(i['url']) #extracts the url from the dictionary
+                
+                for i in review_links: #iterates through links to individual pages
+                    print("Still working...")
+                    link = requests.get(i, headers) #retrieves the URLs
+                    page_content = link.content #extracts URL contents from individual film pages
+                    soup = BeautifulSoup(page_content, 'html.parser') #parses the film page
+                    for i in soup.find_all(class_="what-to-know__section-body"): #looks for the tagging for critics consensus
+                        i = i.get_text() #extracts only the text int he paragraph
+                        i = re.sub("Read critic reviews", "", i)
+                        i = re.sub("\n", " ", i)
+                        i = re.sub("\"", "", i)
+                        i = re.sub("--", "-", i)
+                        preview.append(i)
+                    for c in soup.find("title"): #extracts page title
+                        c = re.sub("- Rotten Tomatoes", "", c) #resubs everything else than film's name
+                        c = re.sub("Rotten Tomatoes: Movies", "", c)
+                        c = re.sub("--", "-", c)
+                        titles.append("mv_title "+c+" mv_title")
+                for i, (k, z) in enumerate(zip(titles, preview)):
+                    f.write("\""+k)
+                    f.write(z+"\", ")
+                print("Finished.")
+                f.close()
+                question = input("Which year do you want to scrape? a)2016 b)2017 c)2018 d)2019 e)2020?")
         
-        for i in review_links: #iterates through links to individual pages
-            print("Still working...")
-            link = requests.get(i) #retrieves the URLs
-            page_content = link.content #extracts URL contents from individual film pages
-            soup = BeautifulSoup(page_content, 'html.parser') #parses the film page
-            for i in soup.find_all(class_="what-to-know__section-body"): #looks for the tagging for critics consensus
-                i = i.get_text() #extracts only the text int he paragraph
-                i = re.sub("Read critic reviews", "", i)
-                i = re.sub("\n", " ", i)
-                i = re.sub("\"", "", i)
-                preview.append(i)
-            for c in soup.find("title"): #extracts page title
-                c = re.sub("- Rotten Tomatoes", "", c) #resubs everything else than film's name
-                titles.append("mv_title"+c+" mv_title")
-            time.sleep(1)
-        f.write("[\n")
-        for i, (k, y, z) in enumerate(zip(titles, review_links, preview)):
-            f.write("\""+k)
-            f.write(z+"\", ")
-            #print("Film: ", k, "\nURL:", y, "\nPreview: ", z, "\n*")
-        print("Finished.")
-        print()
-        f.write("]")
-        f.close()        
 main()
